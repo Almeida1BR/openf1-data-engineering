1332d71eaad7f097977d80e6af01761f9e1f8c3e
Almeida1BR <luis.almeida@faculdadegran.edu.br>
2026-08-29 21:25:13 -0300

    feat: Implementa a automatização da extração

--- src/ingestion/ingestion_openf1.py
+++ src/ingestion/ingestion_openf1.py
@@ -6,23 +6,60 @@ BRONZE_PATH = Path("data/bronze")
 BRONZE_PATH.mkdir(parents=True, exist_ok=True)
 
 BASE_URL = "https://api.openf1.org/v1"
-endpoint = "sessions"
-response = requests.get(f'{BASE_URL}/{endpoint}', params =
 
-    {
-
-    })
-
-data = response.json()
+SESSION_KEY = 9158
 
 
-def save_bronze(endpoint, data):
-    endpoint_path = BRONZE_PATH/endpoint
+def save_bronze(endpoint, data, file_name):
+    endpoint_path = BRONZE_PATH / endpoint
     endpoint_path.mkdir(parents=True, exist_ok=True)
-    file_path = endpoint_path/"data.json"
-    with open(file_path, 'w', encoding='utf-8') as arquivo:
-        json.dump(data, arquivo, ensure_ascii = False, indent=4)
 
-save_bronze(endpoint,data)
-    
+    file_path = endpoint_path / file_name
 
+    with open(file_path, "w", encoding="utf-8") as arquivo:
+        json.dump(
+            data,
+            arquivo,
+            ensure_ascii=False,
+            indent=4
+        )
+
+response = requests.get(
+    f"{BASE_URL}/drivers",
+    params={
+        "session_key": SESSION_KEY
+    }
+)
+
+response.raise_for_status()
+
+drivers = response.json()
+
+for driver in drivers:
+
+    driver_number = driver["driver_number"]
+    full_name = driver["full_name"]
+
+    print(f"Buscando telemetria de {full_name}...")
+
+    response = requests.get(
+        f"{BASE_URL}/car_data",
+        params={
+            "driver_number": driver_number,
+            "session_key": SESSION_KEY
+        }
+    )
+
+    response.raise_for_status()
+
+    data = response.json()
+
+    file_name = f"driver_{driver_number}.json"
+
+    save_bronze(
+        "car_data",
+        data,
+        file_name
+    )
+
+    print(f"{full_name} salvo em {file_name}")
